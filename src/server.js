cc2d1048d683bb2adb75dfa6b755ec14b34682d6
Sreenija <118006659+SreenijaPuvvula@users.noreply.github.com>
2025-10-04 11:07:56 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -41,7 +41,7 @@ const createApp = () => {
   // CORS
   app.use(cors({
     origin: process.env.NODE_ENV === 'production' 
-      ? process.env.CORS_ORIGIN || "https://skycrm-frontend-1.onrender.com"
+      ? process.env.CORS_ORIGIN //|| "https://skycrm-frontend-1.onrender.com"
       : ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173'],
     credentials: true,
     methods: ['GET','POST','PUT','DELETE','PATCH','OPTIONS'],
